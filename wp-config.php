2127695c3f791121668bbcb378db767efeec577d
jadeye <newebgen@gmail.com>
2016-01-03 15:32:30 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'expiria.netdb');
+define('DB_NAME', 'expiriadb');
 
 /** MySQL database username */
 define('DB_USER', 'root');
