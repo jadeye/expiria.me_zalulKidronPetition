a084450b6c8c406cdb8bd29f4e2c9c55e7c01ff7
jadeye <newebgen@gmail.com>
2016-01-03 15:23:35 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'expiria.netdb');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'yehuda72');
+define('DB_PASSWORD', 'xxx');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
